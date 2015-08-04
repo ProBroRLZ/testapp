eae0630ac4f3359541a6f721cad5dbfd0aba5424
ProBroRLZ <pranavnutalapati@gmail.com>
2015-08-04 17:08:40 +0530

    test

--- radius.js
+++
@@ -1,6 +0,0 @@
-document.onload = function(){
-var x = document.getElementsByClassName("form")[0].clientHeight*13/100;
-console.log(x);
-document.getElementsByClassName("form")[0].style.borderRadius = x + "px"; 
-
-}
