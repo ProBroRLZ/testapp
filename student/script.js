a3544e522b79fccdac91c9c8eae1ddb96552df83
ProBroRLZ <pranavnutalapati@gmail.com>
2015-08-04 17:06:35 +0530

    update commit 2

--- student/script.js
+++ student/script.js
@@ -1,5 +1,18 @@
-document.onload = function(){
-	var logo = document.getElementById("logo")
-	var container = document.getElementById("logocontainer")
-	container.style.height = logo.clientHeight
+var header;
+var height;
+function loadup(){
+    header = document.getElementById("header");
+    height = document.getElementById("logocontainer").clientHeight;
+console.log(height);
+console.log(header);
+}
+function test(){
+    
+    
+    if(window.pageYOffset > height){
+        header.classList.add("scroll");
+    }
+    else{
+        header.classList.remove("scroll");
+    }
 }
